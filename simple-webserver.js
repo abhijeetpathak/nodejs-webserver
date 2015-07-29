45b1d2d827811d7feecac39486dc05c5d4ed2049
Abhijeet Pathak <abhijeet.pathak@live.com>
2015-07-29 23:16:03 +0530

    Serves static website from a folder
    Serves static website from a folder. Also sends correct mime type for
    some file types.

--- simple-webserver.js
+++ simple-webserver.js
@@ -1,10 +1,44 @@
 var http = require('http'),
+	fs = require('fs'),
+	path = require('path'),
 	host = '127.0.0.1',
-	port = 9000;
+	port = 9000,
+	sitePath = "./sampleStaticSite";
+
+var mimeTypes = {
+	".htm"	: "text/html",
+	".html"	: "text/html",
+	".txt"	: "text/text",
+	".png"	: "image/png",
+	".gif"	: "image/gif",
+	".jpg"	: "image/jpeg",
+	".jpeg"	: "image/jpeg"
+};
 
 http.createServer(function(req, res) {
-	res.writeHead(200);
-	res.end("Hello World!");
+	
+	var fileName = req.url == "/" ? (sitePath + "/index.htm") : (sitePath + req.url);
+	
+	fs.exists(fileName, function(fileExists){
+		if(fileExists) {
+			var ext = path.extname(fileName);
+			
+			fs.readFile(fileName, function(error, content) {
+				if(error) {
+					res.writeHead(500);
+					res.end('Internal server error.');
+				} else {
+					res.writeHead(200, {'Content-Type': mimeTypes[ext]});
+					res.end(content);
+				}
+				
+			});
+		} else {
+			res.writeHead(404);
+			res.end('File not found.');
+		}
+	});
+	
 }).listen(port, host, function() {
 	console.log("Listening on http://" + host + ":" + port);
 });
